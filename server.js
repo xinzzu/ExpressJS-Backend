c89302528c64ea9f0bb6e786fd32720af65b3f71
xinzzu <azisnurrahman54@gmail.com>
2023-07-13 23:34:54 +0700

    new

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8000; // Adjust the port as needed
+const port = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(express.json());
 
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    dbName: process.env.MONGODB_DB
   })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -23,12 +25,9 @@ mongoose
     console.error('Error connecting to MongoDB:', error);
   });
 
-// Middleware
-app.use(express.json());
-
 // Routes
-app.use('/api/', mahasiswaRouter);
-app.use('/api/', userRouter);
+app.use('/api', mahasiswaRouter);
+app.use('/api', userRouter);
 
 // Start the server
 app.listen(port, () => {
