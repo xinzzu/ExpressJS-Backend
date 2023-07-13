be3043e5b4be36325c3a517650971e9db033e004
xinzzu <azisnurrahman54@gmail.com>
2023-07-14 01:38:07 +0700

    bismillah last

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const mahasiswaRouter = require('../ExpressJS/routers/mahasiswarouters');
-const userRouter = require('../ExpressJS/routers/users');
-const cors = require('cors');
-require('dotenv').config();
+  const express = require('express');
+  const mongoose = require('mongoose');
+  const mahasiswaRouter = require('../ExpressJS/routers/mahasiswarouters');
+  const userRouter = require('../ExpressJS/routers/users');
+  const cors = require('cors');
+  require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 8000;
+  const app = express();
+  const port = process.env.PORT || 8000;
 
-app.use(cors());
-app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: process.env.MONGODB_DB
-  })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+  // Connect to MongoDB
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: process.env.MONGODB_DB
+    })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
+
+  // Routes
+  app.use('/api', mahasiswaRouter);
+  app.use('/api', userRouter);
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
   });
-
-// Routes
-app.use('/api', mahasiswaRouter);
-app.use('/api', userRouter);
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
